1ef844481f88f12af964a8a4a0b97bb1546be926
NaifZaghmout zaghmout <naif-zaghmout@hotmail.com>
2023-12-13 14:07:30 +0000

    initialize custom user manager in api-user models

--- bloodbackend/user_api/models.py
+++ bloodbackend/user_api/models.py
@@ -1,3 +1,24 @@
+"""
+Database models
+"""
 from django.db import models
+from django.contrib.auth.base_user import BaseUserManager
 
-# Create your models here.
+class AppUserManager(BaseUserManager):
+    """
+    User management
+    """
+
+    def create_user(self, email, password=None):
+        """
+        Create the user
+        """
+        if not email:
+            raise ValueError("An email is required.")
+        if not password:
+            raise ValueError("A password is required.")
+        email = self.normalize_email(email)
+        user = self.model(email=email)
+        user.set_password(password)
+        user.save()
+        return user
