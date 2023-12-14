fcb0031d25a21fb5903526d1d6ffc5752464be17
NaifZaghmout zaghmout <naif-zaghmout@hotmail.com>
2023-12-14 15:33:30 +0000

    add user register view for user registration

--- bloodbackend/user_api/views.py
+++ bloodbackend/user_api/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+"""
+Views for the app
+"""
+from django.contrib.auth import get_user_model, login, logout
+from rest_framework.authentication import SessionAuthentication
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .serializers import UserRegisterSerializer
+from rest_framework import permissions, status
+from .validations import custom_validation
 
-# Create your views here.
+class UserRegister(APIView):
+    """
+    User Register
+    """
+    permission_classes = (permissions.AllowAny,)
+
+    def post(self, request):
+        """
+        Post
+        """
+        clean_data = custom_validation(request.data)
+        serializer = UserRegisterSerializer(data=clean_data)
+        if serializer.is_valid(raise_exception=True):
+            user = serializer.create(clean_data)
+            if user:
+                return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(status=status.HTTP_400_BAD_REQUEST)
