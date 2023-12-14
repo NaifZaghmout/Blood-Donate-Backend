dae6668865a28b19bcb44b9873ffc3fe60c2cf32
NaifZaghmout zaghmout <naif-zaghmout@hotmail.com>
2023-12-14 15:56:32 +0000

    add userlogin view to handle user authentication process

--- bloodbackend/user_api/views.py
+++ bloodbackend/user_api/views.py
@@ -5,7 +5,11 @@ from django.contrib.auth import get_user_model, login, logout
 from rest_framework.authentication import SessionAuthentication
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from .serializers import UserRegisterSerializer
+from .serializers import (
+    UserRegisterSerializer,
+    UserLoginSerializer,
+    UserSerializer,
+    PatientBloodSerializer,)
 from rest_framework import permissions, status
 from .validations import custom_validation
 
@@ -26,3 +30,26 @@ class UserRegister(APIView):
             if user:
                 return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(status=status.HTTP_400_BAD_REQUEST)
+
+
+
+class UserLogin(APIView):
+    """
+    User Login
+    """
+    permission_classes = (permissions.AllowAny,)
+    authentication_classes = (SessionAuthentication,)
+
+    
+    def post(self, request):
+        """
+        Post
+        """
+        data = request.data
+        assert validate_email(data)
+        assert validate_password(data)
+        serializer = UserLoginSerializer(data=data)
+        if serializer.is_valid(raise_exception=True):
+            user = serializer.check_user(data)
+            login(request, user)
+            return Response(serializer.data, status=status.HTTP_200_OK)
