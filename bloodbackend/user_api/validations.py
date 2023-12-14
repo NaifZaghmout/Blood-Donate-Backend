c9acb3f6eb87f2e4f99cbfd4e2fc7c794f502f31
NaifZaghmout zaghmout <naif-zaghmout@hotmail.com>
2023-12-14 13:08:36 +0000

    add userregisterserializer for user registration

---
+++ bloodbackend/user_api/validations.py
@@ -0,0 +1,30 @@
+"""
+Django App Serializers
+"""
+from rest_framework import serializers
+from django.core.exceptions import ValidationError
+from django.contrib.auth import get_user_model, authenticate
+from .models import PatientBlood
+
+UserModel = get_user_model()
+
+
+class UserRegisterSerializer(serializers.ModelSerializer):
+    """
+    User Register
+    """
+
+    class Meta:
+        model = UserModel
+        fields = "__all__"
+
+    def create(self, clean_data):
+        """
+        Create
+        """
+        user_obj = UserModel.objects.create_user(
+            email=clean_data["email"], password=clean_data["password"]
+        )
+        user_obj.username = clean_data["username"]
+        user_obj.save()
+        return user_obj
