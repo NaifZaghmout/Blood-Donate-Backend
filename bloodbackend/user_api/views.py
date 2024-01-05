cc0daf6bf7274223712ef9513ea49e93269896f5
NaifZaghmout <naif-zaghmout@hotmail.com>
2024-01-05 15:54:10 +0300

    logout api issue fixed

--- bloodbackend/user_api/views.py
+++ bloodbackend/user_api/views.py
@@ -73,7 +73,13 @@ class UserLogout(APIView):
     """
     permission_classes = (permissions.AllowAny,)
     authentication_classes = ()
-
+    
+    def post(self, request):
+        """
+        Post
+        """
+        logout(request)
+        return Response(status=status.HTTP_200_OK)
 
 
 
