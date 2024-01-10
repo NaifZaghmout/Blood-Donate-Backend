ee813f0fb6cd92d936fc297b660c999e6b94c2ff
NaifZaghmout zaghmout <naif-zaghmout@hotmail.com>
2024-01-10 13:55:03 +0000

    fixed the admin panel login

--- bloodbackend/user_api/models.py
+++ bloodbackend/user_api/models.py
@@ -19,9 +19,9 @@ class AppUserManager(BaseUserManager):
             raise ValueError("An email is required.")
         if not password:
             raise ValueError("A password is required.")
-        email = self.normalize_email(email[0])
-        user = self.model(email=email, username=username[0])
-        user.set_password(str(password[0]))
+        email = self.normalize_email(email)
+        user = self.model(email=email, username=username)
+        user.set_password(password)
         user.save()
         return user
 
