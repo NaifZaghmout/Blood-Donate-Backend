c09e960b57f13560d5e9eb2002c330122e288aa1
NaifZaghmout zaghmout <naif-zaghmout@hotmail.com>
2024-01-18 14:50:11 +0000

    added urls for the profile api and in settings

---
+++ bloodbackend/Profile/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from .views import UserProfileUpdate,UserProfileDetail
+
+urlpatterns = [
+    path('userproile-detail/<int:user_id>/', UserProfileDetail.as_view(), name='userprofile-detail'),
+     path('update-profile/<int:user_id>/', UserProfileUpdate.as_view(), name='userprofile-update'),
+    
+]
