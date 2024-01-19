aaa3a580121d7b820825d5a423f2437893428da4
NaifZaghmout zaghmout <naif-zaghmout@hotmail.com>
2024-01-19 11:50:30 +0000

    userprofile model is registerd with django admin

--- bloodbackend/user_api/admin.py
+++ bloodbackend/user_api/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from .models import AppUser, PatientBlood
+from Profile.models import UserProfile
 # Register your models here.
 admin.site.register(AppUser)
 admin.site.register(PatientBlood)
+admin.site.register(UserProfile)
